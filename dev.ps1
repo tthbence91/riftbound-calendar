90e22dd0c78676cd28edc6797abf879d18a616c9
tthbence91 <tthbence91@gmail.com>
2026-06-24 09:11:24 +0200

    Starting scrpt upgrade

--- dev.ps1
+++ dev.ps1
@@ -9,3 +9,6 @@ Start-Sleep -Milliseconds 500
 wt `
   new-tab --title "API" --tabColor "#1e3a5f" -d $root powershell -NoExit -Command "dotnet run --project RiftboundCalendar.Api --launch-profile http" `; `
   new-tab --title "Web" --tabColor "#1a5c2a" -d $root powershell -NoExit -Command "dotnet run --project RiftboundCalendar.Web"
+
+Start-Sleep -Seconds 4
+Start-Process "chrome.exe" "--incognito http://localhost:5253"
